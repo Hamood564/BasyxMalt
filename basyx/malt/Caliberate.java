5a2700a0f22059fa95dfa64df5af4725708f0830
Hamood564 <72498970+Hamood564@users.noreply.github.com>
2021-08-20 08:20:24 +0100

    Update Caliberate.java

--- basyx/malt/Caliberate.java
+++ basyx/malt/Caliberate.java
@@ -1,5 +1,26 @@
 package org.eclipse.basyx.malt;
 
+import org.eclipse.basyx.aas.manager.ConnectedAssetAdministrationShellManager;
+import org.eclipse.basyx.aas.registration.proxy.AASRegistryProxy;
+import org.eclipse.basyx.submodel.metamodel.api.ISubmodel;
+import org.eclipse.basyx.submodel.metamodel.api.submodelelement.ISubmodelElement;
+
 public class Caliberate {
+	
+	public static void main(String[] args) {
+		// Create Manager
+		ConnectedAssetAdministrationShellManager manager =
+				new ConnectedAssetAdministrationShellManager(new AASRegistryProxy(Server.REGISTRYPATH));
+
+		// Retrieve submodel
+		ISubmodel submodel = manager.retrieveSubmodel(Server.MALTAASID, Server.DOCUSMID);
+
+		// Retrieve Caliberation Property
+		ISubmodelElement maltCaliberate = submodel.getSubmodelElement(Server.MALTCALIBERATE);
+
+		// Print value
+		System.out.println(maltCaliberate.getIdShort() + " is " + maltCaliberate.getValue());
+	}
 
 }
+
