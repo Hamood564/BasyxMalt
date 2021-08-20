0575d3f0d5c334a7ec27fa980445daea9285248b
Hamood564 <72498970+Hamood564@users.noreply.github.com>
2021-08-20 08:19:59 +0100

    Update Options.java

--- basyx/malt/Options.java
+++ basyx/malt/Options.java
@@ -1,5 +1,24 @@
 package org.eclipse.basyx.malt;
 
+import org.eclipse.basyx.aas.manager.ConnectedAssetAdministrationShellManager;
+import org.eclipse.basyx.aas.registration.proxy.AASRegistryProxy;
+import org.eclipse.basyx.submodel.metamodel.api.ISubmodel;
+import org.eclipse.basyx.submodel.metamodel.api.submodelelement.ISubmodelElement;
+
 public class Options {
+	public static void main(String[] args) {
+		// Create Manager
+		ConnectedAssetAdministrationShellManager manager =
+				new ConnectedAssetAdministrationShellManager(new AASRegistryProxy(Server.REGISTRYPATH));
+
+		// Retrieve submodel
+		ISubmodel submodel = manager.retrieveSubmodel(Server.MALTAASID, Server.DOCUSMID);
+
+		// Retrieve Options Property
+		ISubmodelElement maltOptions = submodel.getSubmodelElement(Server.MALTOPTIONS);
+
+		// Print value
+		System.out.println(maltOptions.getIdShort() + " is " + maltOptions.getValue());
+	}
 
 }
