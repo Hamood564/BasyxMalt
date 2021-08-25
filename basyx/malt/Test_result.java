cf876e1bb8d550d2e80c4d3cc0cae3dc9f7eda3e
Hamood564 <72498970+Hamood564@users.noreply.github.com>
2021-08-25 16:23:56 +0100

    Update Test_result.java

--- basyx/malt/Test_result.java
+++ basyx/malt/Test_result.java
@@ -5,8 +5,10 @@ import org.eclipse.basyx.aas.registration.proxy.AASRegistryProxy;
 import org.eclipse.basyx.submodel.metamodel.api.ISubmodel;
 import org.eclipse.basyx.submodel.metamodel.api.submodelelement.ISubmodelElement;
 
-public class Test_result {
-	public static void main(String[] args) {
+public class Test_result implements Runnable{
+	
+	@Override
+	public void run() {
 		// Create Manager
 		ConnectedAssetAdministrationShellManager manager =
 				new ConnectedAssetAdministrationShellManager(new AASRegistryProxy(Server.REGISTRYPATH));
@@ -21,8 +23,7 @@ public class Test_result {
 		// Print value
 		System.out.println(maltResult.getIdShort() + " is " + maltResult.getValue());
 		System.out.println(maltLastdata.getIdShort() + " is " + maltLastdata.getValue());
-		
-		 
+
 	}
 	
 
